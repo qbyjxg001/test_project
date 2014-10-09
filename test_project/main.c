27f740a206747fa96e6bd69ba9c0d4cea5560866
刘义 <liuyi@liuyideMacBook-Air.local>
2014-10-09 14:23:00 +0800

    Initial Commit

---
+++ test_project/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  test_project
+//
+//  Created by 刘义 on 14-10-9.
+//  Copyright (c) 2014年 刘义. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
